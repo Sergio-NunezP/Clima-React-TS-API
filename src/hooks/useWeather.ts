9be6ea861876521a51ec1acae9d3fc0b9a58beaf
Sergio Núñez <sergioop722@gmail.com>
2025-02-22 16:58:33 -0500

    se crea el .env para el API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -5,7 +5,7 @@ export default function useWeather() {
 
     const fetchWeather = async (search: SearchType) => {
 
-        const appId = 'f082993aba3617fcb6a9c7b0165ab5e2'
+        const appId = import.meta.env.VITE_API_KEY
         try {
             const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
